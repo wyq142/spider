f9ca97cb10d1329801f485f96188ab3ef4464fce
wyq142 <51935791+wyq142@users.noreply.github.com>
2019-06-20 00:29:53 +0800

    Add files via upload

--- 1170426024.王宇清.py
+++ 1170426024.王宇清.py
@@ -1,8 +1,8 @@
-data = urllib.parse.urlencode({'wd':'呵呵'})
-print(data)
-url = 'http://www.baidu.com/s?' + data
-response = urllib.request.urlopen(url)
-HTML = response.read().decode('utf8')
-print(HTML)
+def addIndexForSite(site):
+    if isFile(site):
+        return site 
+        bs = getBaseUr
+    return "%s/index.html" % (bs)
+
 
 
